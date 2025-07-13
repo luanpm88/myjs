51e9becd249aa980ddc70eb29c685b331f005815
Luan Pham <luanpm88@gmail.com>
2025-07-13 13:54:15 +0700

    load all templates

--- src/main.js
+++ src/main.js
@@ -7,7 +7,17 @@ window.corejs = {}
 window.corejs.router = new Router(document.getElementById('app'))
 window.corejs.view = view;
 window.corejs.helper = helper;
+window.corejs.views = {};
 
+// Load all views
+const templates = import.meta.glob('/src/views/**/*.ejs', { as: 'raw', eager: true });
+for (const path in templates) {
+  // Normalize path, e.g. '/src/views/component/header.ejs' → 'component/header'
+  const viewName = path.replace('/src/views/', '').replace('.ejs', '');
+  window.corejs.views[viewName] = templates[path];
+}
+
+// Initialize the router and set up event listeners
 document.addEventListener('DOMContentLoaded', () => {
   // On first load
   corejs.router.init();
