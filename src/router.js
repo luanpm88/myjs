a63d2a704700e6f5be8ee97932a092862b4b4893
Luan Pham <luanpm88@gmail.com>
2025-07-14 12:34:47 +0700

    try catch error

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { HomeController } from './controllers/HomeController.js'
 import { TaskController } from './controllers/TaskController.js'
+import { TestController } from './controllers/TestController.js'
 import { helper } from './helper.js'
 
 export const Router = class {
@@ -40,13 +41,25 @@ export const Router = class {
 
     // Render the view for the given path
     render(path, container) {
+        //
+        if (!this.routes[path]) {
+            alert(`Route not found: ${path}. Make sure the path is defined in the src/router.js.`);
+            throw new Error(`Route not found: ${path}. Make sure the path is defined in the src/router.js.`);
+        }
+
         this.routes[path]?.().then(html => {
             helper.innerHTML(container, html)
         });
     }
 
-    // Run controller method
+    // Run controller method via route path
     run(path, request = {}) {
+        //
+        if (!this.routes[path]) {
+            alert(`Route not found: ${path}. Make sure the path is defined in the src/router.js.`);
+            throw new Error(`Route not found: ${path}. Make sure the path is defined in the src/router.js.`);
+        }
+
         // Call the controller method for the given path
         return this.routes[path]?.(request)
     }
